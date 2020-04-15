960d2652892bd6219b1c8d177028312c83cf18e6
xornent@outlook.com <46891834+Xornent@users.noreply.github.com>
2020-04-15 08:52:24 +0800

    relative nav bar sizing

--- js/navigation.js
+++ js/navigation.js
@@ -2,6 +2,11 @@ function global_nav_click(e) {
     gnav_clearall();
     e.parentNode.setAttribute("class", "Gnav-menu-item has-submenu is-open");
     e.setAttribute("aria-expanded", "True");
+    var doc = e.parentNode.children[1];
+    var rect = doc.getBoundingClientRect();
+    var display = window.innerWidth - rect.left;
+    var delta = rect.width - display;
+    doc.style.left = -delta - 18;
 }
 
 function gnav_clearall() {
@@ -20,4 +25,13 @@ function gnav_clearall() {
     var tool = document.getElementById("gnav.tool");
     tool.parentNode.setAttribute("class", "Gnav-menu-item has-submenu");
     tool.setAttribute("aria-expanded", "False");
+
+    var doc = document.getElementById("ul.blog");
+    doc.style.left = 0;
+    doc = document.getElementById("ul.proj");
+    doc.style.left = 0;
+    doc = document.getElementById("ul.doc");
+    doc.style.left = 0;
+    doc = document.getElementById("ul.help");
+    doc.style.left = 0;
 }
